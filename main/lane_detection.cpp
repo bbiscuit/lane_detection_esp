bd7a7bde8772ab589201846f703365097fccf187
Andrew Huffman <55068248+bbiscuit@users.noreply.github.com>
2024-02-19 22:23:30 -0500

    Put the camera task into its own header file. (#18)
    * remove unused routines/variables
    
    * more unused stuff
    
    * Move camera-related stuff into its own file.

--- main/lane_detection.cpp
+++ main/lane_detection.cpp
@@ -33,110 +33,17 @@
 
 // In-project imports
 #include "thread_safe_queue.h"
-
-
-// Special GPIO pins for the AI-THINKER board.
-// Presumably, these work for the ESP32-CAM-MB
-// daughter board as well.
-#define CAMERA_MODEL_AI_THINKER
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+#include "common.h"
+#include "camera_task.h"
 
 
 static char TAG[]="lane_detection";
-
-
-// The parameters which are sent to a task.
-struct TaskParameters
-{
-    // The queue from which the task should get frames.
-    ThreadSafeQueue<cv::Mat>* in;
-
-    // The queue to which the task should output frames.
-    ThreadSafeQueue<cv::Mat>* out;
-
-    // The maximum number of frames which are allowed to
-    // be written into the output.
-    uint8_t max_out_size;
-};
 
 
 // This is necessary because it allows ESP-IDF to find the main function,
 // even though C++ mangles the function name.
 extern "C" {
 void app_main(void);
-}
-
-
-/// @brief Configures the ESP-32-CAM.
-void config_cam()
-{
-    camera_config_t config;
-    config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer = LEDC_TIMER_0;
-    config.pin_d0 = Y2_GPIO_NUM;
-    config.pin_d1 = Y3_GPIO_NUM;
-    config.pin_d2 = Y4_GPIO_NUM;
-    config.pin_d3 = Y5_GPIO_NUM;
-    config.pin_d4 = Y6_GPIO_NUM;
-    config.pin_d5 = Y7_GPIO_NUM;
-    config.pin_d6 = Y8_GPIO_NUM;
-    config.pin_d7 = Y9_GPIO_NUM;
-    config.pin_xclk = XCLK_GPIO_NUM;
-    config.pin_pclk = PCLK_GPIO_NUM;
-    config.pin_vsync = VSYNC_GPIO_NUM;
-    config.pin_href = HREF_GPIO_NUM;
-    config.pin_sscb_sda = SIOD_GPIO_NUM;
-    config.pin_sscb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn = PWDN_GPIO_NUM;
-    config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
-    config.pixel_format = PIXFORMAT_RGB565; 
-    config.frame_size = FRAMESIZE_96X96;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-
-    esp_err_t err = esp_camera_init(&config);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Camera initialization failed");
-        // Handle error
-    }
-
-}
-
-
-/// @brief Gets a frame from the camera
-/// @return The camera frame, as an OpenCV matrix.
-cv::Mat get_frame()
-{
-    // Take a picture from the camera.
-    camera_fb_t *fb = esp_camera_fb_get();
-    if (!fb) {
-        ESP_LOGE(TAG, "Camera capture failed");
-        return cv::Mat();
-    }
-    
-    // Build the OpenCV matrix.
-    // CV_8UC2 is two-channel color, with 8-bit 
-    cv::Mat result(fb->height, fb->width, CV_8UC2, fb->buf);
-
-    // Cleanup and return.
-    esp_camera_fb_return(fb);
-    return result;
 }
 
 
@@ -178,7 +85,7 @@ void task_img_usb(void* arg)
     const TickType_t WAIT_PERIOD = 10;
 
     // Break-out params.
-    TaskParameters* args = static_cast<TaskParameters*>(arg);
+    lane_detect::TaskParameters* args = static_cast<lane_detect::TaskParameters*>(arg);
     ThreadSafeQueue<cv::Mat>* in_q = args->in;
 
     cv::Mat frame;
@@ -197,33 +104,6 @@ void task_img_usb(void* arg)
         send_frame(frame);
 
         vTaskDelay(TASK_PERIOD /*- elapsed_ticks */);
-    }
-}
-
-
-void task_get_img_matrix(void* arg)
-{
-    const TickType_t TASK_PERIOD = 30;
-    const TickType_t WAIT_PERIOD = 10;
-
-    // Extract params
-    TaskParameters* args = static_cast<TaskParameters*>(arg);
-    ThreadSafeQueue<cv::Mat>* out_q = args->out;
-    const uint8_t max_frames = args->max_out_size;
-
-    while (true)
-    {
-        if (max_frames <= out_q->size())
-        {
-            vTaskDelay(WAIT_PERIOD);
-            out_q->pop();
-            continue;
-        }
-
-        auto frame = get_frame();
-        out_q->push(frame);
-
-        vTaskDelay(TASK_PERIOD);
     }
 }
 
@@ -253,7 +133,7 @@ void canny_and_disp(void* arg)
     const TickType_t WAIT_PERIOD = 10;
 
     // Extract params
-    TaskParameters* args = static_cast<TaskParameters*>(arg);
+    lane_detect::TaskParameters* args = static_cast<lane_detect::TaskParameters*>(arg);
     ThreadSafeQueue<cv::Mat>* in_q = args->in;
     ThreadSafeQueue<cv::Mat>* out_q = args->out;
     const uint8_t max_out_size = args->max_out_size;
@@ -306,18 +186,18 @@ void canny_and_disp(void* arg)
 
 void app_main(void)
 {
-    config_cam();
+    lane_detect::config_cam();
 
     // Setup the IT-Queues.
     ThreadSafeQueue<cv::Mat> raw_frame_queue;
     ThreadSafeQueue<cv::Mat> canny_queue;
 
     // Setup the task which gets frames from the camera.
-    TaskParameters get_frames_params = {nullptr, &raw_frame_queue, 1};
-    xTaskCreate(task_get_img_matrix, "get_img_matrix", 4096, &get_frames_params, 1, nullptr);
+    lane_detect::TaskParameters get_frames_params = {nullptr, &raw_frame_queue, 1};
+    xTaskCreate(lane_detect::camera_task, "get_img_matrix", 4096, &get_frames_params, 1, nullptr);
 
     // Start the canny thread on the main processor.
-    TaskParameters task_canny_and_disp_params = {&raw_frame_queue, &canny_queue, 1};
+    lane_detect::TaskParameters task_canny_and_disp_params = {&raw_frame_queue, &canny_queue, 1};
     canny_and_disp(&task_canny_and_disp_params);
 
 }
