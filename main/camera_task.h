b65037fc7b4e84ed0052bc92f15721dd9e877aba
Andrew Huffman <ahuffman@cedarville.edu>
2024-02-22 16:54:16 -0500

    Decl of new get_frame.

--- main/camera_task.h
+++ main/camera_task.h
@@ -5,8 +5,12 @@
 /// Author: Andrew Huffman
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-
 #pragma once
+
+#undef EPS
+#include "opencv2/core.hpp"
+#define EPS 192
+#include "esp_camera.h"
 
 namespace lane_detect
 {
@@ -18,4 +22,12 @@ namespace lane_detect
     /// @param arg Should be of type "TaskParameters." The input queue is not used in this task;
     /// only the output and the max output num.
     void camera_task(void* arg);
+
+
+    /// @brief Gets a frame from the ESP-32 camera and interprets it as an OpenCV matrix.
+    /// @param fb The frame buffer. If this is not nullptr, this will be freed back to the ESP-32
+    /// cam prior to overwriting.
+    /// @param mat The matrix into which the frame is read. Note that the data from `fb` was not
+    /// copied; just the reference. So if fb is freed this Mat is invalidated.
+    cv::Mat get_frame(camera_fb_t* fb);
 }
