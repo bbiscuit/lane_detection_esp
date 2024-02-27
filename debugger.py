048efff4ae864f1e002efbaba7f0a1dc5017d8a5
Andrew Huffman <ahuffman@cedarville.edu>
2024-02-26 21:01:56 -0500

    Added ability to override default.

--- debugger.py
+++ debugger.py
@@ -6,9 +6,11 @@ import threading
 import queue
 import functools
 import json
+import sys
 
 def disp_threshold_frame(frame: cv2.Mat, thresh_color: dict, win_name: str):
     """"""
+
 
 def read_frame(s: serial.Serial) -> tuple[cv2.Mat, str]:
     """Reads a frame from the serial port. Returned with it is the type of the frame,
@@ -100,10 +102,12 @@ def setup_color_thresh_window(window_name: str, thresh_color: dict):
     cv2.createTrackbar("Saturation", window_name, thresh_color["saturation"], 255, functools.partial(on_trackbar, color_to_update=thresh_color, dim="saturation"))
     cv2.createTrackbar("Value", window_name, thresh_color["value"], 255, functools.partial(on_trackbar, color_to_update=thresh_color, dim="value"))
 
+
 def load_settings(filename: str) -> dict:
     """Loads settings for the app from the given json file."""
     with open(filename, 'r') as f:
         return json.load(f)
+
 
 def main():
     """The main subroutine."""
@@ -119,6 +123,10 @@ def main():
     #s.timeout = 0.5
     s.setDTR(False)
     s.setRTS(False)
+
+    if len(sys.argv) > 1:
+        s.port = sys.argv[1]
+
     s.open()
 
     # The BGR threshold for the image.
