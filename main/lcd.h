bfea8d3052f54e57d7990627db4f2069bb549a21
Andrew Huffman <ahuffman@cedarville.edu>
2024-03-30 17:50:28 -0400

    Document the lcd functions.

--- main/lcd.h
+++ main/lcd.h
@@ -22,6 +22,15 @@
 
 namespace lane_detect
 {
+    /// @brief Writes a number of lines to the LCD screen, from top to bottom.
+    /// @param screen The screen to write to.
+    /// @param lines The lines to write to the screen.
     void write_val_lcd(SSD1306_t& screen, std::vector<std::string>& lines);
+
+
+    /// @brief Writes an OpenCV matrix to the screen.
+    /// @param screen The screen to write to.
+    /// @param bin_mat The matrix to write. Should be a binary mask.
+    /// @param vert_bar If this is a positive value, writes a vertical bar in one column on the screen.
     void write_bin_mat(SSD1306_t& screen, const cv::Mat& bin_mat, const int vert_bar = -1);
 }
