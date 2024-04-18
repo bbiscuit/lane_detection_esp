85e37d3cb99e3e49390009d6f2280d6a027af5ec
Andrew Huffman <ahuffman@cedarville.edu>
2024-04-18 15:40:16 -0400

    Generate file

--- main/params.h
+++ main/params.h
@@ -10,7 +10,7 @@
 
 constexpr uint8_t expected_line_pos = 81;
 
-constexpr uint8_t expected_red_y = 78;
+constexpr uint8_t expected_red_y = 82;
 constexpr uint8_t expected_red_radius = 2;
 
 constexpr uint8_t outside_thresh_min_hue = 8;
@@ -21,9 +21,9 @@ constexpr uint8_t outside_thresh_max_hue = 179;
 constexpr uint8_t outside_thresh_max_sat = 255;
 constexpr uint8_t outside_thresh_max_val = 255;
 
-constexpr uint8_t outside_cropping_top = 45;
+constexpr uint8_t outside_cropping_top = 51;
 constexpr uint8_t outside_cropping_bottom = 0;
-constexpr uint8_t outside_cropping_left = 60;
+constexpr uint8_t outside_cropping_left = 22;
 constexpr uint8_t outside_cropping_right = 0;
 
 constexpr uint16_t outside_min_detect_area = 139;
